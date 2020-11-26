1c5e76f8dbfcd6e773ce05958f3c8ebd4b4601f4
Nobody <Havila2k@gmail.com>
2020-11-26 04:23:21 +0000

    Add api route to read json

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,6 +47,10 @@ return [
             'driver' => 'local',
             'root' => storage_path('app'),
         ],
+        'local-storage' => [
+            'driver' => 'local',
+            'root' => storage_path('files'),
+        ],
 
         'public' => [
             'driver' => 'local',
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Cache;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/prices', function () {
+    //Caching since the file doesn't get changed often
+    return Cache::remember('price-json', 360, function () {
+        return Storage::disk('local-storage')->get('live_prices.json');
+    });
 });
